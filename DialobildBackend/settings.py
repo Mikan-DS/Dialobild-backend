431c211ed0c8f0e362230f4fd1c7746ed37fb6c1
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-02-28 21:04:57 +0300

    CORS rule added

--- DialobildBackend/settings.py
+++ DialobildBackend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'colorfield',
     'users.apps.UsersConfig',
     'API.apps.ApiConfig',
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +128,5 @@ AUTH_USER_MODEL = 'users.User'
 OAUTH2_PROVIDER = {
     "OIDC_ENABLED": True,
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
