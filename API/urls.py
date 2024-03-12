34898414ca6cca9e869704242e9dc81a449fb9a5
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-03-12 20:38:26 +0300

    - RAW NODE INPUT endpoint

--- API/urls.py
+++ API/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('project/id/<int:project_id>/', views.get_full_project_by_id, name='get_full_project_by_id'),
     path('project/name/<str:project_name>/', views.get_full_project_by_name, name='get_full_project_by_name'),
     path('project/save/', views.save_project, name='save_project'),
+    path('project/add_raw_nodes/', views.add_raw_nodes, name='add_raw_nodes'),
 ]
