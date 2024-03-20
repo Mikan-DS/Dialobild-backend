0be7bf05fb891eee888ca2261b8e63f45c129b47
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-03-20 11:49:48 +0300

    - insert frontend

---
+++ frontend/update-frontend-part.py
@@ -0,0 +1,22 @@
+import os
+import glob
+import shutil
+
+# Запускаем npm run build в директории Dialobild-Frontend
+if os.path.basename(os.getcwd()) != 'Dialobild-Frontend':
+    os.chdir('Dialobild-Frontend')
+os.system('npm run build')
+
+# Получаем список файлов в директории
+js_files = glob.glob('build/static/js/*.js')
+
+# Копируем файлы в нужную директорию
+for file in js_files:
+    shutil.copy(file, '../static/js/dialobuild-app.js')
+
+# Получаем список файлов в директории
+css_files = glob.glob('build/static/css/*.css')
+
+# Копируем файлы в нужную директорию
+for file in js_files:
+    shutil.copy(file, '../static/css/dialobuild-app.css')
